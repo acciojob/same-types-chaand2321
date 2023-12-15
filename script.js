f94d9d2410b9812a94b387236bf19f7dd2072e99
chandruMurthy <113250768+chaand2321@users.noreply.github.com>
2023-12-15 10:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function isSameType(value1, value2) {
   //your js code here
-	if(value1 == 'Nan' && value2 == 'Nan'){
-		return ("true") ;
+	// if(value1 == 'Nan' && value2 == 'Nan'){
+	// 	return ("true") ;
 		
-	} else if(typeof(value1)==typeof(value2) ){
+	// } else 
+	if(typeof value1 ===typeof value2 ){
 		return ("true");
 	}else{
 		return ("false") ;
